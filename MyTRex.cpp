224db1511dfb8c181024640c810b94b05d1c7aa6
Michaël Braem <michaelbraem94@gmail.com>
2014-12-12 11:14:50 +0100

    De functies toegevoegd aan MyTRex.cpp

--- MyTRex.cpp
+++ MyTRex.cpp
@@ -8,8 +8,18 @@ namespace TRexLib{
      * @i2c the I2C representing the bus on which the slave is connected.
      * @i2cAddress the address of the slave
      */
-    TRex::TRex(I2C* i2c, int i2cAddress){
-        this->i2c = i2c;
-        this->i2cAddress = i2cAddress;
+    MyTRex::MyTRex(I2C* i2c, int i2cAddress):Trex(i2c,i2cAddress)
+    {
+        //this->i2c = i2c;
+        //this->i2cAddress = i2cAddress;
+    }
+
+    MyTrex::readStatus(MyStatusDataPacket * status)
+    {
+        return false;
+    }
+    writeCommand(CommandDataPacket * command)
+    {
+        return false;
     }
 }
